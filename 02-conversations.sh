93a0b5db2cb7731b7fb6e8a153e43ea06db39e19
prudvirajn <nadupalliprudviraj@gmail.com.com>
2025-06-30 16:56:26 +0530

    adding variables

--- 02-conversations.sh
+++ 02-conversations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Raj"  : Hi, Nani how are you?
-echo "Nani" : Hi, Raj Im doing good. what about you?
-echo "Raj"  : Great! what else?
-echo "Nani" : Nothing!
+echo "Raj"  :: Hi, Nani how are you?
+echo "Nani" :: Hi, Raj Im doing good. what about you?
+echo "Raj"  :: Great! what else?
+echo "Nani" :: Nothing! i just started learning Devops.
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1= Raj
+PERSON2= Nani
+
+echo "{PERSON1}"  :: Hi, Nani how are you?
+echo "{PERSON2}" :: Hi, Raj Im doing good. what about you?
+echo "{PERSON1}"  :: Great! what else?
+echo "{PERSON2}" :: Nothing! i just started learning Devops.
